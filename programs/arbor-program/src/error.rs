f7a4859c6a053206e25a560b9feb121bb88e6fc8
SwineCoder101 <liamfaruq@gmail.com>
2025-05-07 00:46:38 +0100

    add-top-up-order

--- programs/arbor-program/src/error.rs
+++ programs/arbor-program/src/error.rs
@@ -5,4 +5,12 @@ use anchor_lang::prelude::*;
 pub enum ArborError {
     #[msg("Health factor too low")]
     HealthTooLow,
+    #[msg("Signer is not the owner of the order, cannot close order")]
+    UnAuthorizedCloseOrder,
+    #[msg("Signer is not the owner of the order, cannot claim yield")]
+    UnAuthorizedClaimYield,
+    #[msg("Signer is not the owner of the order, cannot top up order")]
+    UnAuthorizedTopUpOrder,
+    #[msg("client error: Invalid side, please check the side of the order")]
+    InvalidSide,
 }
