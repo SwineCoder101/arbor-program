8851558d0d344693f3f6df715b3689f8b33ece0b
Liam Faruq <liam@Liams-MacBook-Pro.local>
2025-05-16 20:21:38 +0100

    add-simulation

--- simulate/2_create_order_trader.ts
+++ simulate/2_create_order_trader.ts
@@ -1,7 +1,17 @@
+import { PublicKey } from "@solana/web3.js";
 import { initClient, logBalances } from "./util";
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function main() {
   const { client, trader } = await initClient();
+
+  // Load account info to get USDC mint
+  const accountInfoPath = path.join(__dirname, 'account_info.json');
+  const accountInfo = JSON.parse(fs.readFileSync(accountInfoPath, 'utf8'));
+
+  await client.setGlobalConfig();
+
   
   console.log("Creating order...");
   const orderAddress = await client.createOrder({
